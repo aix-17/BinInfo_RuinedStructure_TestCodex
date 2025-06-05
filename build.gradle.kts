f3830d0cbf1554e1b159cac8d0d8b4e7e785b66f
aix-17 <dr435505@gmail.com>
2025-06-05 19:28:29 +0200

    Enable Hilt plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,6 @@ plugins {
     id("com.android.library") version "8.8.0" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
-//    id("com.google.dagger.hilt.android") version "2.56.2" apply false
+    id("com.google.devtools.ksp") version "2.1.21-2.0.1" apply false
+    id("com.google.dagger.hilt.android") version "2.56.2" apply false
 }
